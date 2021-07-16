aceb2363955976ecac6fd62ac912723a2e14d7af
Sigurdur Arnason <shamaroth@gmail.com>
2021-07-16 00:27:43 +0000

    implemented all tweets display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+//import './App.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import {Card} from "./components"
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Card";
+import Col from "react-bootstrap/Card";
+import Row from "react-bootstrap/Card";
 
 const App = () => {
   const [stateTweets,setTweets] = useState([]);
@@ -24,21 +27,27 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">  
-
-      {stateTweets && stateTweets.map((item, index) => (
-                <Card 
-                  bg="light"
-                  border="dark"
-                  text="dark"
-                  key={index}
-                  style={{ width: '18rem' }}
-                >
-                  <Card.Body>
-                    <Card.Text>{item.content}</Card.Text>
-                  </Card.Body>
-                  
-                </Card>
-              ))}
+      <Container>
+        <Row>
+          <Col md="auto">
+            {stateTweets && stateTweets.map((item, index) => (
+              <Card 
+                bg="light"
+                border="dark"
+                text="dark"
+                key={index}
+                style={{ width: '18rem' }}
+              >
+                <Card.Body>
+                  <Card.Title>{item.user.userName}</Card.Title>
+                  <Card.Text>{item.content}</Card.Text>
+                </Card.Body>
+                
+              </Card>
+            ))}
+          </Col>
+        </Row>
+      </Container>
       </header>
     </div>
   );
